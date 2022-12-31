78da4d80ccc91f71e3d6cadf285284a691b17c22
Matthew <mattj20@gmail.com>
2022-12-31 13:33:54 -0800

    Created User component and added some test code

---
+++ src/components/users/User.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class User extends Component {
+
+    render() {
+        return(
+            <h1>Hi!</h1>
+        )
+    }
+}
+
+export default User;
+
