7ff8ef379750ac194a19cb65a92acba072f5ba79
Matthew <mattj20@gmail.com>
2022-12-31 12:39:41 -0800

    Updated users controller to have params and handle a post request to create a user, renders json

--- catching-up-api/app/controllers/users_controller.rb
+++ catching-up-api/app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.new(user_params)
+        user.save
+        render json: user, except: [:created_at, :updated_at]
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username)
+    end
 end
