37a13f8725d6b75d107688c0c065777958187edf
Matthew <mattj20@gmail.com>
2022-12-31 21:53:38 -0800

    Added route and imports for store and routes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import User from './components/users/User'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// const store = createStore()
+
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <Provider> */}
+    <Router>
+      <Route exact path="/" component={User} />
+      <App />
+    </Router>
+    {/* </Provider> */}
   </React.StrictMode>
 );
