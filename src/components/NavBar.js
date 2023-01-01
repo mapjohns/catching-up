e469749170975fd4d7fa7358387d5a0a4782fc22
Matthew <mattj20@gmail.com>
2022-12-31 21:55:58 -0800

    Created NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+import { NavLink } from "react-router-dom";
+
+
+class NavBar extends Component {
+
+    render() {
+        return(
+
+        )
+    }
+
+}
