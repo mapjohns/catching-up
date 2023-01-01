fd73232f0e1154455452fd0919a30a4918570434
Matthew <mattj20@gmail.com>
2022-12-31 16:14:09 -0800

    Added form and state

---
+++ src/components/users/UserInput.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+
+class UserInput extends Component {
+
+    state = {
+        username: "",
+        password: ""
+    }
+
+    render() {
+        return(
+        <div>
+            <form>
+                <input />
+                <input />
+            </form>
+        </div>
+        )
+    }
+
+}
+
+export default UserInput
