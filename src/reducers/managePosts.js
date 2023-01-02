154f1c3d621aab367928baa6c082955a416d46aa
Matthew <mattj20@gmail.com>
2023-01-01 17:16:52 -0800

    Created reducer for Posts and added ADD_POST

---
+++ src/reducers/managePosts.js
@@ -0,0 +1,11 @@
+
+export default function managePosts(state = { posts: [] }, action) {
+    switch(action.type) {
+        case "ADD_POST":
+        debugger
+            return {...state, posts: state.posts.concat("TEST")};
+        default:
+            return state
+    }
+    return state
+}
