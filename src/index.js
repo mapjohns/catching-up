e3e1529fe67395b047bd5e7a29175f42e64d36fa
Matthew <mattj20@gmail.com>
2023-01-01 17:38:08 -0800

    Updated to use a store

--- src/index.js
+++ src/index.js
@@ -6,17 +6,18 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import User from './components/users/User'
+import managePosts from './reducers/managePosts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// const store = createStore()
+const store = createStore(managePosts)
 
 root.render(
   <React.StrictMode>
-    {/* <Provider> */}
+    <Provider store={store}>
     <Router>
       <Route exact path="/" component={User} />
       <App />
     </Router>
-    {/* </Provider> */}
+    </Provider>
   </React.StrictMode>
 );
