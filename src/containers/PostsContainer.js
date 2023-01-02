8ec113515f1937610e13a553cdbc67f0d47df346
Matthew <mattj20@gmail.com>
2023-01-01 17:07:23 -0800

    Created PostsContainer

---
+++ src/containers/PostsContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+
+class PostsContainer extends Component {
+
+    render() {
+        return (
+          <div>
+            <PostInput  />
+          </div>
+        )
+      }
+
+}
+
+export default PostsContainer
