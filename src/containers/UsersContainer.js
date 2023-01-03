ea52efa2fedbcfb31fa253d287c7c85455fe9a3d
Matthew <mattj20@gmail.com>
2023-01-02 18:36:47 -0800

    Updated to dispatchprops and utilize action function for async

--- src/containers/UsersContainer.js
+++ src/containers/UsersContainer.js
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import UserInput from "../components/users/UserInput";
+import { createUser } from '../actions/userActions'
 
 class UsersContainer extends Component {
     
     render() {
         return (
           <div>
-            <UserInput />
+            <UserInput returnUser={this.props.createUser}/>
           </div>
         )
       }
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return { createUser: username => dispatch({type: "RETURNING_USER", username: username})}
+    return { createUser: username => dispatch(createUser({username})) }
 }
 
-
-export default connect(mapDispatchToProps)(UsersContainer)
+export default connect(null, mapDispatchToProps)(UsersContainer)
