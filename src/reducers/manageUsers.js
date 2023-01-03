cf710f18d048970e8a387ce3703d003544807ae4
Matthew <mattj20@gmail.com>
2023-01-02 16:21:30 -0800

    Created manageUsers reducer

---
+++ src/reducers/manageUsers.js
@@ -0,0 +1,14 @@
+const usersReducer = (state = { users: [] }, action) => {
+    switch (action.type) {
+      case "RETURNING_USER":
+        debugger
+        return {
+          ...state,
+          users: [...state.users],
+        };
+      default:
+        return state;
+    }
+  };
+  
+  export default usersReducer;
