86bbb6bbf7032655278bae6ddfab67e110c6af86
Matthew <mattj20@gmail.com>
2023-01-02 16:10:05 -0800

    Updated for thunk

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import User from './components/users/User'
 import managePosts from './reducers/managePosts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const store = createStore(managePosts)
+const store = createStore(managePosts, applyMiddleware(thunk))
 
 root.render(
   <React.StrictMode>
