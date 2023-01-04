e27078bf8f2e962d276aa7fccc87daa37d193df8
Matthew <mattj20@gmail.com>
2023-01-03 18:41:23 -0800

    Renamed case and update return to add more information and in object form

--- src/reducers/managePosts.js
+++ src/reducers/managePosts.js
@@ -1,10 +1,8 @@
-
 export default function managePosts(state = { posts: [] }, action) {
     switch(action.type) {
-        case "ADD_POST":
+        case "ADD_POST_STATE":
         // debugger
-
-            return {...state, posts: state.posts.concat(action.message)};
+            return {...state, posts: state.posts.concat({user: {id: action.post.user_id, username: action.post.user.username}, message: action.post.message})};
         default:
             return state
     }
