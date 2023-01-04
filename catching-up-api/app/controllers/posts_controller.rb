75986922a751dab6a35beb306f75d23cec30f6c4
Matthew <mattj20@gmail.com>
2023-01-03 17:28:49 -0800

    Added create and post_params

--- catching-up-api/app/controllers/posts_controller.rb
+++ catching-up-api/app/controllers/posts_controller.rb
@@ -1,2 +1,12 @@
 class PostsController < ApplicationController
+
+    def create
+        binding.pry
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:message, :user_id)
+    end
 end
