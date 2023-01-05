dc2ab1c2e46ec61b5343a2e373c206016b66f9be
Matthew <mattj20@gmail.com>
2023-01-04 19:11:29 -0800

    Added sessionscontroller to handle users logging in

--- catching-up-api/app/controllers/sessions_controller.rb
+++ catching-up-api/app/controllers/sessions_controller.rb
@@ -1,7 +1,11 @@
 class SessionsController < ApplicationController
 
     def create
-        binding.pry
+        user = User.find_by(:username => params[:user][:username])
+        if user && !!user.authenticate(params[:user][:password])
+            render json: user, except: [:created_at, :updated_at]
+        end
+        
     end
 
 end
