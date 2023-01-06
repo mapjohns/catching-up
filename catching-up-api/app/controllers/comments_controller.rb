5b79a716e567179e7d0e12cd87f2c76b74340a2e
Matthew <mattj20@gmail.com>
2023-01-05 21:34:57 -0800

    Added create to handle creation of a comment and renders json, comment_params also created

--- catching-up-api/app/controllers/comments_controller.rb
+++ catching-up-api/app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+
+    def create
+        comment = Comment.new(comment_params)
+        comment.save
+        render json: comment, include: [:user, :post], except: [:created_at, :updated_at]
+
+    end
+
+    private
+    
+    def comment_params
+        params.require(:comment).permit(:content, :user_id, :post_id)
+    end
 end
