45d0f1486bf5be469eb7673ff6aefd23976aeac0
Matthew <mattj20@gmail.com>
2023-01-05 19:04:58 -0800

    Passes down postId

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react"
 import Post from "./Post"
+// import CommentsContainer from "../../containers/CommentsContainer"
 
 class Posts extends Component {
 
-    renderPosts = () => this.props.posts.map(post => <Post key={post.user} post={post.message} username={post.user.username} />)
+    renderPosts = () => this.props.posts.map(post => <Post match={this.props.match} key={`post${post.post_id}`} post={post.message} username={post.user.username} postId={post.post_id}/>)
 
     render() {
         // debugger
