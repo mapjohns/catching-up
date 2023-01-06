7acf6aefb41f7fb4b226b0cf8a2ea3e1b5f68022
Matthew <mattj20@gmail.com>
2023-01-05 22:16:28 -0800

    Passes postID to CommentsContainer

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import CommentInput from "../comments/CommentInput";
+import CommentsContainer from "../../containers/CommentsContainer";
 
 class Post extends Component {
 
@@ -11,6 +12,7 @@ class Post extends Component {
             <div>
             <h4>{this.props.username}</h4>
             <p>{this.props.post}</p>
+            <CommentsContainer postID={this.props.postId}/>
             <Link key={`post${this.props.postId}`} to={`${this.props.match.url}/all/post/${this.props.postId}`}>
               Reply
             </Link>
