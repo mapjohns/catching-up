97439cc9496e0fedc0e943866bd9c924cefb769c
Matthew <mattj20@gmail.com>
2023-01-05 19:31:16 -0800

    Filled out the form, added state, and added handlers

--- src/components/comments/CommentInput.js
+++ src/components/comments/CommentInput.js
@@ -4,11 +4,29 @@ import { connect } from 'react-redux';
 
 class CommentInput extends Component {
 
+    state = {
+        content: ""
+    }
 
+    onChangeHandler = (event) => {
+        this.setState({
+            content: event.target.value
+        })
+    }
+
+    onSubmitHandler = (event) => {
+        event.preventDefault()
+        this.setState({
+            content: ""
+        })
+    }
 
     render() {
         return(
-            
+            <form>
+                <input onChange={this.onChangeHandler} value={this.state.content} type="text"/>
+                <input  type="submit"/>
+            </form>
         )
 
     }
