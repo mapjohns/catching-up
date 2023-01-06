7c1153771f23942f9eb80a4b3cbbce8843c6c354
Matthew <mattj20@gmail.com>
2023-01-05 19:06:33 -0800

    Imports react-router-dom and now has a Reply button along with a route parg to CommentInput

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class Post extends Component {
 
@@ -8,6 +10,10 @@ class Post extends Component {
             <div>
             <h4>{this.props.username}</h4>
             <p>{this.props.post}</p>
+            <Link key={`post${this.props.postId}`} to={`${this.props.match.url}/all/post/${this.props.postId}`}>
+              Reply
+            </Link>
+            <Route path={`${this.props.match.url}/all/post/${this.props.postId}`} render={() => <CommentInput /> } />
             </div>
         )
     }
