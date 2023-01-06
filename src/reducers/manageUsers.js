e1f0ac0211fea99a0ca04043f14d83373450088b
Matthew <mattj20@gmail.com>
2023-01-05 17:57:17 -0800

    Added logout action

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,11 +1,15 @@
 const usersReducer = (state = { users: [] }, action) => {
     switch (action.type) {
       case "SAVE_USER_ID":
-        debugger
         return {
           ...state,
           users: [action.user.id],
         };
+      case "LOG_OUT_USER":
+        return {
+          ...state,
+          users: [],
+        }
       default:
         return state;
     }
