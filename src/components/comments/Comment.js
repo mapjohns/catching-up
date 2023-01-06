bb2007db7cae4efc377e333c5c8c9ebdc3734ca1
Matthew <mattj20@gmail.com>
2023-01-05 22:29:37 -0800

    Renders comment content

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react"
 
 class Comment extends Component {
-    
+
+    render() {
+        return(
+            <p>{this.props.content}</p>
+        )
+    }
+
 }
 
 export default Comment
