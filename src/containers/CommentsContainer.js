7dbb03181678afb522938d1aec476b709731cf1b
Matthew <mattj20@gmail.com>
2023-01-05 22:30:00 -0800

    Removed unneeded code

--- src/containers/CommentsContainer.js
+++ src/containers/CommentsContainer.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
-import CommentInput from "../components/comments/CommentInput";
 import Comments from "../components/comments/Comments";
 
 class CommentsContainer extends Component {
