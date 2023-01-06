5ed37bbc4eb08e0e81b8bd91bca3ca6f19bdba1e
Matthew <mattj20@gmail.com>
2023-01-05 19:07:53 -0800

    Sends post_id

--- src/reducers/managePosts.js
+++ src/reducers/managePosts.js
@@ -2,7 +2,7 @@ export default function managePosts(state = { posts: [] }, action) {
     switch(action.type) {
         case "ADD_POST_STATE":
         // debugger
-            return {...state, posts: state.posts.concat({user: {id: action.post.user_id, username: action.post.user.username}, message: action.post.message})};
+            return {...state, posts: state.posts.concat({user: {id: action.post.user_id, username: action.post.user.username}, message: action.post.message, post_id: action.post.id})};
         default:
             return state
     }
