ba07fb945e9bd141e326b06cd6fdfe8f8225263c
Matthew <mattj20@gmail.com>
2023-01-07 12:21:45 -0800

    UPDATE_POST_STATE added to update specific post in state

--- src/reducers/managePosts.js
+++ src/reducers/managePosts.js
@@ -2,7 +2,22 @@ export default function managePosts(state = { posts: [] }, action) {
     switch(action.type) {
         case "ADD_POST_STATE":
         // debugger
-            return {...state, posts: state.posts.concat({user: {id: action.post.user_id, username: action.post.user.username}, message: action.post.message, post_id: action.post.id})};
+            return {...state, posts: state.posts.concat({user: {id: action.post.user_id, 
+                                                         username: action.post.user.username}, 
+                                                         message: action.post.message, 
+                                                         post_id: action.post.id})};
+        case "UPDATE_POST_STATE":
+            // debugger
+            return  {...state, posts: state.posts.map((post) => {if (post.post_id === action.post.id) 
+                                                                    {post.message = action.post.message
+                                                                        return post}
+                                                                    else {
+                                                                        return post
+                                                                    }
+                                                                }
+                                                        )
+                                                            }
+        ;
         default:
             return state
     }
