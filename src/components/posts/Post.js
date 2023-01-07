fe1a70d34f97f1222ec3085748f7d7b78e33c5b8
Matthew <mattj20@gmail.com>
2023-01-07 11:21:00 -0800

    Added link and edit prop

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import CommentInput from "../comments/CommentInput";
 import CommentsContainer from "../../containers/CommentsContainer";
+import PostInput from "./PostInput";
 
 class Post extends Component {
 
@@ -11,7 +12,10 @@ class Post extends Component {
             <div>
             <h4>{this.props.username}</h4>
             <p>{this.props.post}</p>
-            {/* <Route path={`${this.props.match.url}/all/post/${this.props.postId}/edit`} render={() => <PostInput  postID={this.props.postId} /> } /> */}
+            <Link key={`post${this.props.postId}edit`} to={`${this.props.match.url}/all/post/${this.props.postId}/edit`}>
+            Edit
+            </Link>
+            <Route path={`${this.props.match.url}/all/post/${this.props.postId}/edit`} render={() => <PostInput postID={this.props.postId} edit={"Edit"}/> } /><br></br>
 
             <Route path={`${this.props.match.url}/all/post/${this.props.postId}/comments`} render={() => <CommentsContainer postID={this.props.postId}/>} />
             <Link key={`post${this.props.postId}comments`} to={`${this.props.match.url}/all/post/${this.props.postId}/comments`}>
