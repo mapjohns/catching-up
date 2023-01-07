76e96d0553bbde65a17f8e2aab874ea2aba81865
Matthew <mattj20@gmail.com>
2023-01-07 10:39:45 -0800

    Changed, so that it exports both functions, editPost added

--- src/actions/postAction.js
+++ src/actions/postAction.js
@@ -1,4 +1,4 @@
-export function createPost(test) {
+function createPost(test) {
     let newVar = test
     // debugger
     return (dispatch) => {
@@ -19,3 +19,27 @@ export function createPost(test) {
         .then((post) => dispatch( {type: "ADD_POST_STATE", post: post }))
     }
 }
+
+function editPost(test) {
+    let newVar = test
+    debugger
+    return (dispatch) => {
+        fetch(`http://localhost:3000/posts/${123}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+                body: JSON.stringify({
+                    post: {
+                    message: test.message,
+                    user_id: test.user
+                    }
+                })
+            })
+        .then(resp => resp.json())
+        .then((post) => dispatch( {type: "ADD_POST_STATE", post: post }))
+    }
+}
+
+export { createPost, editPost };
