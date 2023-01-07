8eff8cfd8a1bb6e08f9229f72f8aa20dda590ce8
Matthew <mattj20@gmail.com>
2023-01-07 10:44:58 -0800

    Updated formatting to be more readable

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -4,10 +4,15 @@ import Post from "./Post"
 
 class Posts extends Component {
 
-    renderPosts = () => this.props.posts.map(post => <Post match={this.props.match} key={`post${post.post_id}`} post={post.message} username={post.user.username} postId={post.post_id}/>)
+    renderPosts = () => this.props.posts.map(post => 
+                        <Post match={this.props.match} 
+                              key={`post${post.post_id}`} 
+                              post={post.message} 
+                              username={post.user.username} 
+                              postId={post.post_id}/>
+                              )
 
     render() {
-        // debugger
         return (
         this.renderPosts()
         )
