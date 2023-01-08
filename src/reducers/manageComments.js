06c8a95a379a6bd076f8a4762fdfef67bc1d356a
Matthew <mattj20@gmail.com>
2023-01-07 22:15:07 -0800

    Added ADD_COMMENTS_STATE to add comments to state

--- src/reducers/manageComments.js
+++ src/reducers/manageComments.js
@@ -1,7 +1,7 @@
 export default function manageComments(state = { comments: [] }, action) {
     switch(action.type) {
         case "ADD_COMMENT_STATE":
-        debugger
+        // debugger
         const comment = {
             commentID: action.comment.id,
             userID: action.comment.user_id,
@@ -9,7 +9,18 @@ export default function manageComments(state = { comments: [] }, action) {
             content: action.comment.content,
             username: action.comment.user.username
         }
-        return {...state, comments: state.comments.concat(comment)}
+        return {...state, comments: state.comments.concat(comment)};
+
+        case "ADD_COMMENTS_STATE":
+            const comments = action.comments.map(comment => ({commentID: comment.id,
+                                                              userID: comment.user_id,
+                                                              postID: comment.post_id,
+                                                              content: comment.content,
+                                                              username: comment.user.username
+            }))
+            // debugger
+            return {...state, comments: state.comments.concat(comments)};
+        ;
         default:
             return state
     }
