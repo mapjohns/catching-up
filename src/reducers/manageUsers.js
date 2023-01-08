5390033d2b530bf517a1aeb8bf91c0adb3faf2c8
Matthew <mattj20@gmail.com>
2023-01-08 14:35:35 -0800

    Added new code to include more information on the user logged in

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,9 +1,16 @@
 const usersReducer = (state = { users: [] }, action) => {
     switch (action.type) {
       case "SAVE_USER_ID":
+        // debugger
         return {
           ...state,
-          users: [action.user.id],
+          // New Code
+          users: {id: action.user.id,
+                  username: action.user.username,
+                  description: action.user.description,
+                  loggedIn: true
+                }
+          // users: [action.user.id],
         };
       case "LOG_OUT_USER":
         return {
