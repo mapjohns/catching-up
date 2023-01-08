1974239becb124f1010726596366186e03b1b521
Matthew <mattj20@gmail.com>
2023-01-07 22:58:28 -0800

    Repurposed to render User

--- src/containers/UsersContainer.js
+++ src/containers/UsersContainer.js
@@ -1,26 +1,19 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
-import UserInput from "../components/users/UserInput";
-import { createUser } from '../actions/userActions';
-import { loginUser } from "../actions/userLoginAction";
+import User from '../components/users/User' 
 
 class UsersContainer extends Component {
     
     render() {
         return (
           <div>
-            Your ID Number is: {this.props.userID}
-            <UserInput returnUser={this.props.createUser} loginUser={this.props.loginUser} logoutUser={this.props.logoutUser}/>
+            Your ID number is: {this.props.userID}
+            <User />
           </div>
         )
       }
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return { createUser: username => dispatch(createUser({username})) ,
-              loginUser: credentials => dispatch(loginUser({credentials})),
-              logoutUser: () => dispatch({type: "LOG_OUT_USER"})}
-}
 
 const mapStateToProps = (state) => {
   return {
@@ -28,5 +21,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer)
+export default connect(mapStateToProps)(UsersContainer)
