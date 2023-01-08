99b67d0696af07e0edd62caab6f5f6ef7944d4c2
Matthew <mattj20@gmail.com>
2023-01-07 21:34:07 -0800

    Created ADD_POSTS_STATE to add existing posts in backend to state

--- src/reducers/managePosts.js
+++ src/reducers/managePosts.js
@@ -1,7 +1,7 @@
 export default function managePosts(state = { posts: [] }, action) {
     switch(action.type) {
         case "ADD_POST_STATE":
-        // debugger
+            // debugger
             return {...state, posts: state.posts.concat({user: {id: action.post.user_id, 
                                                          username: action.post.user.username}, 
                                                          message: action.post.message, 
@@ -20,6 +20,14 @@ export default function managePosts(state = { posts: [] }, action) {
         case "DELETE_POST_STATE":
             // debugger
             return {...state, posts: state.posts.filter((post) => post.post_id !== action.test)};
+
+        case "ADD_POSTS_STATE":
+            // debugger
+            const posts = action.posts.map(post => ({user: {id: post.user_id, 
+                                                  username: post.user.username}, 
+                                                  message: post.message, 
+                                                  post_id: post.id}))
+            return {...state, posts: state.posts.concat(posts)};
         default:
             return state
     }
