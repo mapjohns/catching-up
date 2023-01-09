6715918064f9875b19f5c62244a3784b6e8e84e8
Matthew <mattj20@gmail.com>
2023-01-08 20:00:04 -0800

    Renders UpdateComment

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import UpdateComment from "./UpdateComment";
 
 class Comment extends Component {
 
     render() {
+        // debugger
         return(
+            <div>
             <p>{this.props.username}: {this.props.content}</p>
+            <UpdateComment userID={this.props.userID} postID={this.props.postID} content={this.props.content}/>
+            </div>
         )
     }
 
