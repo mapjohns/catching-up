16c2edbdc3512516b5a57a4d0089aaf6dad59448
Matthew <mattj20@gmail.com>
2023-01-08 16:43:33 -0800

    Renders UpdateUserInfo on a condition

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-
+import UpdateUserInfo from './UpdateUserInfo';
 
 class User extends Component {
 
     render() {
+        // debugger
         return(
+            <div>
             <h1>Hi!</h1>
+            <h2>Username: {this.props.user.username}</h2>
+            {!!this.props.description ? 
+                <h3>About: {this.props.description}</h3> : <UpdateUserInfo user={this.props.user}/>}
+            </div>
         )
     }
 }
