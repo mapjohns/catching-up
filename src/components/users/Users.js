bf68c9ee499e48b5036655910fc0951d515cd8f2
Matthew <mattj20@gmail.com>
2023-01-09 19:56:14 -0800

    Renders route and links for users

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react"
 import User from "./User"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class Users extends Component {
 
-    renderUsers = () => this.props.allUsers.map(user => <User userID={user.user.id}
-                                                              username={user.user.username} 
-                                                              description={user.user.description}/>
+    renderUsers = () => this.props.allUsers.map(user => 
+        <div>
+                <Link key={`User${user.user.id}`} to={`${this.props.match.url}/${user.user.id}`}>
+                {user.user.username}
+                </Link>
+                <Route path={`${this.props.match.url}/${user.user.id}`} render={() => <User userID={user.user.id}  
+                                                                                  username={user.user.username} 
+                                                                                  description={user.user.description}/> } />
+        </div>
+
                                                              )
 
     render() {
-        // debugger
         return(
         this.renderUsers()
         )
