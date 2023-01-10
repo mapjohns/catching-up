10cbda4fb09de07a1bac35997c795cea29ed6ca5
Matthew <mattj20@gmail.com>
2023-01-09 17:35:37 -0800

    Imports User and renders users

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react"
-
+import User from "./User"
 
 class Users extends Component {
 
-    renderUsers = () => this.props.allUsers.map(user => <User /> )
+    renderUsers = () => this.props.allUsers.map(user => <User userID={user.user.id}
+                                                              username={user.user.username} 
+                                                              description={user.user.description}/>
+                                                             )
 
     render() {
+        // debugger
         return(
-        this.renderUsers
+        this.renderUsers()
         )
     }
 
