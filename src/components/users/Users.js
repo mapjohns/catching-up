ea929f7c94377f510b98b6b1824b428f128cb8e2
Matthew <mattj20@gmail.com>
2023-01-10 17:37:22 -0800

    Removed unneeded code and added react bootstrap styling

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,17 +2,25 @@ import React, { Component } from "react"
 import User from "./User"
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 class Users extends Component {
 
     renderUsers = () => this.props.allUsers.map(user => 
         <div>
-                <Link key={`User${user.user.id}`} to={`${this.props.match.url}/${user.user.id}`}>
-                {user.user.username}
-                </Link>
-                <Route exact path={`${this.props.match.url}/${user.user.id}`} render={() => <User userID={user.user.id}  
-                                                                                  username={user.user.username} 
-                                                                                  description={user.user.description}/> } />
+            <Card style={{ width: '30rem' }}>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>
+                            <Link key={`User${user.user.id}`} to={`${this.props.match.url}/${user.user.id}`}>
+                                {user.user.username}
+                            </Link>
+                            <Route exact path={`${this.props.match.url}/${user.user.id}`} render={() => <User userID={user.user.id}  
+                                                                                    username={user.user.username} 
+                                                                                    description={user.user.description}/> } />
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Card>
         </div>
 
                                                              )
@@ -22,11 +30,20 @@ class Users extends Component {
         const loggedUser  = this.props.loggedUser
         return(
             <div>
-                <p>Your Profile</p>
-                <Link key={`LoggedUser${ loggedUser.id}`} to={`${this.props.match.url}/${loggedUser.id}/edit`}> Your Profile </Link>
-                <Route path={`${this.props.match.url}/${loggedUser.id}/edit`} render={() => <User edit={"Edit"} user={loggedUser} userID={loggedUser.id} username={loggedUser.username} description={loggedUser.description} />} />
-                {/* <User loggedUser={loggedUser} /> */}
+                <br></br>
+                <Card style={{ width: '30rem' }}>
+                    <Card.Header>
+                        <Link key={`LoggedUser${ loggedUser.id}`} to={`${this.props.match.url}/${loggedUser.id}/edit`}> My Profile </Link>
+                    </Card.Header>
+                    <Route path={`${this.props.match.url}/${loggedUser.id}/edit`} render={() => <User edit={"Edit"} user={loggedUser} userID={loggedUser.id} username={loggedUser.username} description={loggedUser.description} />} />
+                </Card>
+                <br></br>
+                <br></br>
+                <Card style={{ width: '30rem' }}>
+                <Card.Header>All Users</Card.Header>
+                </Card>
                 {this.renderUsers()}
+
             </div>
         )
     }
