23bff3a233e47c85bca37b19dfd07b749f8c9d7b
Matthew <mattj20@gmail.com>
2023-01-10 18:24:40 -0800

    Updated with react bootstrap styling

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -15,33 +15,33 @@ class Post extends Component {
             <div>
             <Card>
             <Card.Header as="h5">{this.props.username}: {this.props.post}</Card.Header>
-            {this.props.loggedUserID === this.props.userId ? 
-            <div>
-            <Link key={`post${this.props.postId}edit`} to={`${this.props.match.url}/post/${this.props.postId}/edit`}>
-            Edit
-            </Link>
-            <Route path={`${this.props.match.url}/post/${this.props.postId}/edit`} render={() => <PostInput postID={this.props.postId} edit={"Edit"}/> } /><br></br>
-
-            <Link key={`post${this.props.postId}delete`} to={`${this.props.match.url}/post/${this.props.postId}/delete`}>
-            Delete
-            </Link>
-            <Route path={`${this.props.match.url}/post/${this.props.postId}/delete`} render={() => this.props.deletePost(this.props.postId)} />
-            </div>
-            :
-            <div></div>
-            }
 
             <Route path={`${this.props.match.url}/${this.props.postId}/comments`} render={() => <CommentsContainer match={this.props.match} postID={this.props.postId}/>} />
             <Link key={`post${this.props.postId}comments`} to={`${this.props.match.url}/${this.props.postId}/comments`}>
             Show Comments
             </Link>
-            <br></br>
-            
+    
             <Link key={`post${this.props.postId}`} to={`${this.props.match.url}/${this.props.postId}/comments/new`}>
               Reply
             </Link>
             <Route exact path={`${this.props.match.url}/${this.props.postId}/comments/new`} render={() => <CommentInput postID={this.props.postId} /> } />
-            <br></br>
+                {this.props.loggedUserID === this.props.userId ? 
+                <div>
+                <Card.Footer>
+                <Link key={`post${this.props.postId}edit`} to={`${this.props.match.url}/post/${this.props.postId}/edit`}>
+                Edit Post
+                </Link>
+                <Route path={`${this.props.match.url}/post/${this.props.postId}/edit`} render={() => <PostInput postID={this.props.postId} edit={"Edit"}/> } /><br></br>
+
+                <Link key={`post${this.props.postId}delete`} to={`${this.props.match.url}/post/${this.props.postId}/delete`}>
+                Delete Post
+                </Link>
+                <Route path={`${this.props.match.url}/post/${this.props.postId}/delete`} render={() => this.props.deletePost(this.props.postId)} />
+                </Card.Footer>
+                </div>
+                :
+                <div></div>
+                }
             </Card>
             </div>
         )
